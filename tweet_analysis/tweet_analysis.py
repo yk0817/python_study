3471bda956e27e0dc28d0144aaaddcc35b6d8cec
山本憲 <em072029@gmail.com>
2017-04-13 23:08:41 +0900

    python mysql環境構築　機械学習の構想をメモ書き

---
+++ tweet_analysis/tweet_analysis.py
@@ -0,0 +1,38 @@
+# 機械学習プログラムで遊んでみる
+# 1.ツイートテキストを全て取得
+# 2.ユーザーが特定の単語でどんな事をしているのか把握
+# 3.例えば　仕事→楽しい、辛い　など　→特定のキーワードに対してその人がどんな印象を抱いているかわかる
+# ビジュアルはD3で作れたらいいなー
+#coding:utf-8
+import pymysql
+
+#接続情報
+dbh = pymysql.connect(
+         host='localhost',
+         user='root',
+         password='',
+         db='call_music',
+         charset='utf8',
+         cursorclass=pymysql.cursors.DictCursor
+    )
+
+#カーソル
+stmt = dbh.cursor()
+
+#SQL
+sql = "select * from artists"
+
+#実行
+stmt.execute(sql)
+
+#取得
+rows = stmt.fetchall()
+
+#ループ
+for row in rows:
+    print(row)
+
+#掃除
+stmt.close();
+dbh.close();
+
