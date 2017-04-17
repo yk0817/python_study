c5b30093daed60889ab2d2a49e46e28e59cdc660
山本憲 <em072029@gmail.com>
2017-04-17 22:20:50 +0900

    削除

--- tweet_analysis/make_word2vec.py
+++ tweet_analysis/make_word2vec.py
@@ -5,5 +5,5 @@ tokyo_politic_file = 'tokyo.wakati'
 
 # Word2Vecモデル
 data = word2vec.LineSentence(tokyo_politic_file)
-model = word2vec.Word2Vec(data,size=200,window=10,hs=1,min_count=10,sg=1)
+model = word2vec.Word2Vec(data,size=400,window=10,hs=1,min_count=10,sg=1)
 model.save('tokyo_model')
