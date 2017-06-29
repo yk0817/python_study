23954e75b2d76ed07910dd92f3cc40a78850e794
山本憲 <em072029@gmail.com>
2017-06-29 22:21:08 +0900

    コミット

--- tweet_analysis/make_word2vec.py
+++
@@ -1,9 +0,0 @@
-from gensim.models import word2vec
-
-tokyo_politic_file = 'tokyo.wakati'
-
-
-# Word2Vecモデル
-data = word2vec.LineSentence(tokyo_politic_file)
-model = word2vec.Word2Vec(data,size=400,window=10,hs=1,min_count=10,sg=1)
-model.save('tokyo_model')
