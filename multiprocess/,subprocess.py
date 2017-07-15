97128e3598a49e18f8163788c6120f08c7e98dce
山本憲 <em072029@gmail.com>
2017-07-16 00:26:36 +0900

    Subprocess

---
+++ multiprocess/,subprocess.py
@@ -0,0 +1,16 @@
+import subprocess as sub
+
+# ret = sub.getoutput('date')
+# print(ret)
+# 
+# ret2 = sub.getoutput('date -u')
+# print(ret2)
+# 
+# ret3 = sub.getoutput('date -u | wc')
+# print(ret3)
+# 
+# ret4 = sub.check_output(['date', '-u'])
+# print(ret4)
+
+ret5 = sub.call(['date', '-u'])
+ret5
