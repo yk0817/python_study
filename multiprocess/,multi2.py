b5d6579462d3f8fb6440e493daf8ba18095472d8
yamamto ken <em072029@gmail.com>
2023-05-07 09:32:54 +0900

    独学コンピューターサイエンティスト

--- multiprocess/,multi2.py
+++ multiprocess/,multi2.py
@@ -1,20 +1,24 @@
-from multiprocessing import Process,Queue
+from multiprocessing import Process, Queue
 
-def sender(q,n):
+
+def sender(q, n):
     q.put('{0}回目のhello world!'.format(n))
 
+
 def main():
+    global p
     q = Queue()
     for i in range(5):
-        p = Process(target=sender,args=(q,i))
+        p: Process = Process(target=sender, args=(q, i))
         p.start()
     print(q.get())
     print(q.get())
     print(q.get())
     print(q.get())
     print(q.get())
-    
+
     p.join()
+
 
 if __name__ == '__main__':
     main()
